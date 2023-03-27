1e1872662837e3bfe75b95fcbfba6ee9c6e6b50c
DimasCW <114164560+DimasCW@users.noreply.github.com>
2023-03-27 15:20:33 +0700

    Add files via upload

---
+++ Exercise 1.cpp
@@ -0,0 +1,38 @@
+// Exercise 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+double rerata(double test1, double test2) {
+    return (test1 + test2) / 2;
+}
+
+void data(string nama, string status) {
+    cout << "masukkan nama : ";
+    cin >> nama;
+    cout << " Status"
+    if (average >= 70 || test2 >= 80) {
+        cout << "Selamat, Anda diterima!" << endl;
+    }
+    else {
+        cout << "Maaf, Anda tidak diterima." << endl;
+    }
+
+    return 0;
+}
+
+int main() {
+
+
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
